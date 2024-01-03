1dae25a24316dec2461c8b29980c58593731e12f
tbbt0123221 <112914239+tbbt0123221@users.noreply.github.com>
2024-01-03 16:07:26 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.py
@@ -0,0 +1,37 @@
+def on_received_number(receivedNumber):
+    global i, l, re
+    basic.clear_screen()
+    if 0 <= receivedNumber:
+        i = 0
+        for index in range(int(receivedNumber / 5)):
+            led.plot(0, i)
+            led.plot(1, i)
+            led.plot(2, i)
+            led.plot(3, i)
+            led.plot(4, i)
+            i += 1
+        l = 0
+        for index2 in range(receivedNumber % 5):
+            led.plot(l, i)
+            l += 1
+    else:
+        re = receivedNumber * -1
+        i = 4
+        for index3 in range(int(re / 5)):
+            led.plot(0, i)
+            led.plot(1, i)
+            led.plot(2, i)
+            led.plot(3, i)
+            led.plot(4, i)
+            i += -1
+        l = 4
+        for index4 in range(re % 5):
+            led.plot(l, i)
+            l += -1
+    basic.pause(1000)
+radio.on_received_number(on_received_number)
+
+re = 0
+l = 0
+i = 0
+radio.set_group(23)
---
+++ main.ts
@@ -0,0 +1,41 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.clearScreen()
+    if (0 <= receivedNumber) {
+        i = 0
+        for (let index = 0; index < Math.trunc(receivedNumber / 5); index++) {
+            led.plot(0, i)
+            led.plot(1, i)
+            led.plot(2, i)
+            led.plot(3, i)
+            led.plot(4, i)
+            i += 1
+        }
+        l = 0
+        for (let index = 0; index < receivedNumber % 5; index++) {
+            led.plot(l, i)
+            l += 1
+        }
+    } else {
+        re = receivedNumber * -1
+        i = 4
+        for (let index = 0; index < Math.trunc(re / 5); index++) {
+            led.plot(0, i)
+            led.plot(1, i)
+            led.plot(2, i)
+            led.plot(3, i)
+            led.plot(4, i)
+            i += -1
+        }
+        l = 4
+        for (let index = 0; index < re % 5; index++) {
+            led.plot(l, i)
+            l += -1
+        }
+    }
+    basic.pause(10)
+})
+let re = 0
+let l = 0
+let i = 0
+radio.setGroup(11)
+basic.showNumber(0)
